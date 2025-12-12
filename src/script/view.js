bd4f1ef78b5a3b7494139d54b7472961feabd973
Mateus Ferreira <mateusjunior803@gmail.com>
2025-12-12 00:02:07 -0300

    feat: Tornar tela inicial responsiva

--- src/script/view.js
+++ src/script/view.js
@@ -13,7 +13,7 @@ const showLoggedUserNickname = () => {
 const showPendingTaskScore = () => {
   const loggedUserTasks = getUserTaskList();
 
-  const pedingTaskElement = document.querySelector(".profile-header .pending > .heading")
+  const pedingTaskElement = document.querySelector(".cards .pending > .heading")
   const pendingTasks = loggedUserTasks.filter(task => task["isDone"] === false);
 
   pedingTaskElement.textContent = pendingTasks.length;
@@ -22,7 +22,7 @@ const showPendingTaskScore = () => {
 const showDoneTaskScore = () => {
   const loggedUserTasks = getUserTaskList();
 
-  const doneTaskElement = document.querySelector(".profile-header .done > .heading")
+  const doneTaskElement = document.querySelector(".cards .done > .heading")
   const doneTasks = loggedUserTasks.filter(task => task["isDone"] === true);
 
   doneTaskElement.textContent = doneTasks.length;
@@ -58,12 +58,12 @@ const showTaskList = () => {
           <li class="task" data-task-id="${task["id"]}">
               <input type="checkbox" name="task-${task["id"]}" id="${task["id"]}">
               <div class="text">
-                <span class="task">${task["title"]}</span>
+                <span class="task-title">${task["title"]}</span>
                 <span class="date">${task["createdAt"]}</span>
               </div>
-              <button class="remove-btn">
+              <button class="remove-btn" title="Deletar tarefa">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                  <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                  <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
                 </svg>
               </button>
           </li>
@@ -72,12 +72,12 @@ const showTaskList = () => {
         doneTaskArea.insertAdjacentHTML("beforeend", `
           <li class="task" data-task-id="${task["id"]}">
               <div class="text">
-                <span class="task">${task["title"]}</span>
+                <span class="task-title">${task["title"]}</span>
                 <span class="date">${task["createdAt"]}</span>
               </div>
-              <button class="remove-btn">
+              <button class="remove-btn" title="Deletar tarefa">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                  <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                  <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
                 </svg>
               </button>
             </li>
